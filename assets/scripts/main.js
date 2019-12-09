04203a734dc604a22aaf3e7ab4f5287e37fdaebb
xTRBL <gil@xtreblig.com>
2019-12-09 11:14:56 -0500

    Added social nav to all pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var audio = new Audio("assets/audio/StealingFire.mp3");
   var songPath = 'assets/audio/';
-  var playlist = { songOne: 'StealingFire.mp3', songTwo: 'LockMeUp.mp3', songThree: 'LeavingInTheEvening.mp3', songFour: 'TrainDelay.mp3', songFive: 'IThinkIThink.mp3', songSix: 'MaybeItsTheSun.mp3', songSeven: 'Mischevious.mp3', songEight: 'LivingProof.mp3', songNine: 'NoTime.mp3', songTen: 'WickedWar.mp3', songEleven: 'BornaBitch.mp3', songTwelve: 'SomethingCloser.mp3', songThirteen: 'CompulsiveNecessity.mp3', songFourteen: 'Rosemary.mp3', songFifteen: 'RunnedOver.mp3'};
+  var playlist = { songOne: 'StealingFire.mp3', songTwo: 'LockMeUp.mp3', songThree: 'LeavingInTheEvening.mp3', songFour: 'TrainDelay.mp3', songFive: 'IThinkIThink.mp3', songSix: 'MaybeItsTheSun.mp3', songSeven: 'Mischevious.mp3', songEight: 'LivingProof.mp3', songNine: 'NoTime.mp3', songTen: 'WickedWar.mp3', songEleven: 'BornaBitch.mp3', songTwelve: 'SomethingCloser.mp3', songThirteen: 'CompulsiveNecessity.mp3', songFourteen: 'Rosemary.mp3', songFifteen: 'RunnedOver.mp3', fuckTheKing: 'FuckTheKing.mp3'};
   var currentlyPlaying = "songOne";
   var isPlaying = false;
 
@@ -11,6 +11,11 @@ window.onload = function() {
       $('#play-pause-button').removeClass('fa-play');
       $('#play-pause-button').addClass('fa-pause');
       audio.play();
+      if(currentlyPlaying == 'fuckTheKing'){
+        $('.play-button-box').css({'background':"url('/assets/img/about.jpg')", 'background-size':'contain'});
+      }else {
+        $('.play-button-box').css({'background':"url('/assets/img/stealing-fire.jpg')", 'background-size':'contain'});
+      }
       isPlaying = true;
     }
     else
